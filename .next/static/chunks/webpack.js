4965e5c2e287b37239b1a538614261fb4d1d1754
Maurice <bongartzmaurice24@gmail.com>
2026-04-08 00:08:16 +0200

    add delete event functionality

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c353ea63718b4965"; }
+/******/ 		__webpack_require__.h = function() { return "309406ba16be60d2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
